5c2cbd8c9b91f9e4467e71ad9a7605f0082214e4
Kvkens <kvkens@qq.com>
2016-08-31 11:25:13 +0800

    add:添加webpack 配置构建目录.

---
+++ dist/js/neoui-grid.js
@@ -0,0 +1,77 @@
+/** 
+ * neoui-grid v1.0.5
+ * grid
+ * author : yonyou FED
+ * homepage : https://github.com/iuap-design/grid#readme
+ * bugs : https://github.com/iuap-design/grid/issues
+ **/ 
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _Test = __webpack_require__(1);
+	
+	document.write((0, _Test.Test)());
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	function Test() {
+		return "Test";
+	}
+	
+	exports.Test = Test;
+
+/***/ }
+/******/ ]);
+//# sourceMappingURL=neoui-grid.js.map
---
+++ js/Test.js
@@ -0,0 +1,5 @@
+function Test(){
+	return "Test";
+}
+
+export {Test};
