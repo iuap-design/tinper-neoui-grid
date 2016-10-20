bfa688f2cad03a5c287063998ad8b81d080be6c0
liuyk <liuyk@yonyou.com>
2016-10-20 14:36:22 +0800

    fix: pro-IUAPDESIGN-89:友云采：grid没有数据的话下面的边线没有显示

--- dist/js/u-grid.js
+++ dist/js/u-grid.js
@@ -2048,7 +2048,7 @@
 	        $.each(this.dataSourceObj.rows, function (i) {
 	            htmlStr += oThis.createContentOneRow(this, createFlag);
 	        });
-	        htmlStr += this.createContentRowsSumRow();
+	        htmlStr += this.createContentRowsSumRow(createFlag);
 	        htmlStr += '</tbody>';
 	    }
 	    return htmlStr;
